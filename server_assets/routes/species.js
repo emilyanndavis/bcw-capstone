89a608bee6282b726b83faf566dcfcb41d5aafd3
Emily Davis <emilyanndavis@gmail.com>
2016-08-15 15:23:39 -0600

    getByName, getByCategory, getBySize functions work

--- server_assets/routes/species.js
+++ server_assets/routes/species.js
@@ -27,6 +27,58 @@
         })
         .delete(function(req, res){
             return {error: 'You cannot delete a species. What are you, a monster?'};
+        });
+
+    router.route('/species/type/:type')
+        .get(function(req, res){
+            Species.getByCategory(req.params.type, function(species){
+                res.send(species);
+            })
         })
+        .post(function(req, res){
+            return {error: 'You cannot add to this list.'};            
+
+        })
+        .put(function(req, res){
+            return {error: 'You cannot update this list.'};            
+        })
+        .delete(function(req, res){
+            return {error: 'You cannot delete this list.'};
+        });
+
+    router.route('/species/size/:size')
+        .get(function(req, res){
+            Species.getBySize(req.params.size, function(species){
+                res.send(species);
+            })
+        })
+        .post(function(req, res){
+            return {error: 'You cannot add to this list.'};            
+
+        })
+        .put(function(req, res){
+            return {error: 'You cannot update this list.'};            
+        })
+        .delete(function(req, res){
+            return {error: 'You cannot delete this list.'};
+        });  
+
+    router.route('/species/name/:name')
+        .get(function(req, res){
+            Species.getByName(req.params.name, function(species){
+                res.send(species);
+            })
+        })
+        .post(function(req, res){
+            return {error: 'You cannot add to this list.'};            
+
+        })
+        .put(function(req, res){
+            return {error: 'You cannot update this list.'};            
+        })
+        .delete(function(req, res){
+            return {error: 'You cannot delete this list.'};
+        });
+        
 
     }());
