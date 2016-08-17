da71f8f5eaea12500927a45f1fc55c097e26530d
Kurt Hammes <kilowatt11@cableone.net>
2016-08-17 16:10:31 -0600

    map route should now display properly

--- public/app/components/map/map.js
+++ public/app/components/map/map.js
@@ -113,69 +113,74 @@
 
         Initializer.mapsInitialized.
             then(function () {
+               
                 var largeInfowindow = new google.maps.InfoWindow();
+
                 map = new google.maps.Map(document.getElementById('map'), {
                     center: { lat: 43.639056, lng: -116.195673 },
                     zoom: 17,
                     // styles: styles,
-                    mapTypeControl: false
+                    mapTypeControl: false,
+            });
+                    // if(map) {google.maps.event.trigger(map, 'resize');}
+              
+
+
+        var logControlDiv = document.createElement('div');
+        var logControl = new LogControl(logControlDiv, map);
+
+        logControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(logControlDiv);
+
+
+        var fieldGuideControlDiv = document.createElement('div');
+        var fieldGuideControl = new FieldGuideControl(fieldGuideControlDiv, map);
+
+        fieldGuideControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(fieldGuideControlDiv);
+
+        var logBookControlDiv = document.createElement('div');
+        var logBookControl = new LogBookControl(logBookControlDiv, map);
+
+        logBookControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.RIGHT_TOP].push(logBookControlDiv);
+
+
+
+        var infoWindow = new google.maps.InfoWindow({ map: map });
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                    title: "You are here!"
                 });
 
-                var logControlDiv = document.createElement('div');
-                var logControl = new LogControl(logControlDiv, map);
 
-                logControlDiv.index = 1;
-                map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(logControlDiv);
-
-
-                var fieldGuideControlDiv = document.createElement('div');
-                var fieldGuideControl = new FieldGuideControl(fieldGuideControlDiv, map);
-
-                fieldGuideControlDiv.index = 1;
-                map.controls[google.maps.ControlPosition.TOP_RIGHT].push(fieldGuideControlDiv);
-
-                var logBookControlDiv = document.createElement('div');
-                var logBookControl = new LogBookControl(logBookControlDiv, map);
-
-                logBookControlDiv.index = 1;
-                map.controls[google.maps.ControlPosition.RIGHT_TOP].push(logBookControlDiv);
-
-
-
-                var infoWindow = new google.maps.InfoWindow({ map: map });
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(function (position) {
-                        var pos = {
-                            lat: position.coords.latitude,
-                            lng: position.coords.longitude
-                        };
-                        var marker = new google.maps.Marker({
-                            position: pos,
-                            map: map,
-                            title: "You are here!"
-                        });
-
-
-                        infoWindow.setPosition(pos);
-                        infoWindow.setContent(`<div><h1>${'You Are Here!'}</h1><p>Latitude: ${pos.lat}</p><p>Longitude: ${pos.lng}</div>`);
-                        map.setCenter(pos);
-                    }, function () {
-                        handleLocationError(true, infoWindow, map.getCenter());
-                    });
-                } else {
-                    // Browser doesn't support Geolocation
-                    handleLocationError(false, infoWindow, map.getCenter());
-                }
-
+                infoWindow.setPosition(pos);
+                infoWindow.setContent(`<div><h1>${'You Are Here!'}</h1><p>Latitude: ${pos.lat}</p><p>Longitude: ${pos.lng}</div>`);
+                map.setCenter(pos);
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter());
             });
-
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-            infoWindow.setPosition(pos);
-            infoWindow.setContent(browserHasGeolocation ?
-                'Error: The Geolocation service failed.' :
-                'Error: Your browser doesn\'t support geolocation.');
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
         }
 
+    });
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
     }
+
+}
 
 } ());
