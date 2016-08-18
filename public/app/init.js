5d97e31e072f8dd8b25b032749d056325da7d90e
Lindy Crain <lindylcrain@gmail.com>
2016-08-18 09:41:49 -0600

    landing page changes

--- public/app/init.js
+++ public/app/init.js
@@ -60,35 +60,35 @@
 
 
 // WILDLIFE COMPONENT: just an html form used to initially populate the species database
-        .component('wildlife', {
-            controller: WildlifeController,
-            templateUrl: 'app/wildlife.html'
-        });
+        // .component('wildlife', {
+        //     controller: WildlifeController,
+        //     templateUrl: 'app/wildlife.html'
+        // });
 
-        function WildlifeController($http){
-            let $ctrl = this;
+        // function WildlifeController($http){
+        //     let $ctrl = this;
 
-            $ctrl.getSpecies = function(){
-                $http.get('/api/species').then(function(res){
-                    console.log('Number of species in DB: ' + res.data.length);
-                });
-                // console.log('Species coming soon. Check back later!');
-            }
+        //     $ctrl.getSpecies = function(){
+        //         $http.get('/api/species').then(function(res){
+        //             console.log('Number of species in DB: ' + res.data.length);
+        //         });
+        //         // console.log('Species coming soon. Check back later!');
+        //     }
 
-            $ctrl.species = {};
-            $ctrl.species.stateProtected = false;
+        //     $ctrl.species = {};
+        //     $ctrl.species.stateProtected = false;
 
-            $ctrl.addSpecies = function(){
-                console.log('Creating species...');
-                console.log($ctrl.species);
-                $http.post('/api/species', $ctrl.species).then(function(res){
-                    console.log(res.data);
-                    $ctrl.species = {};
-                    $ctrl.species.stateProtected = false;
-                    console.log('Species created!');
-                });
-            }
-        }
+        //     $ctrl.addSpecies = function(){
+        //         console.log('Creating species...');
+        //         console.log($ctrl.species);
+        //         $http.post('/api/species', $ctrl.species).then(function(res){
+        //             console.log(res.data);
+        //             $ctrl.species = {};
+        //             $ctrl.species.stateProtected = false;
+        //             console.log('Species created!');
+        //         });
+        //     }
+        // }
 // END WILDLIFE COMPONENT        
 
         
